5750cd61f65fbd80ca0d28185eaa72f44492cffc
Sam Donnelly <samueldonnelly11@gmail.com>
2022-02-12 15:29:27 -0800

    Added an includes.h files to organize include files and make the code more readable.

---
+++ headers/includes.h
@@ -0,0 +1,28 @@
+/**
+ * @file includes.h
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief One place to organize included files for a project.
+ * 
+ * @version 0.1
+ * @date 2022-02-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef _INCLUDES_H_
+#define _INCLUDES_H_
+
+//=======================================================================================
+// Includes 
+
+#include "HW_125_microSD_driver.h"
+#include "mpu6050_driver.h"
+#include "potentiometer_read.h"
+#include "wayintop_lcd_driver.h"
+
+//=======================================================================================
+
+#endif  // _INCLUDES_H_
