0dc36c0373b4c7da80f530139bc771ca4920311a
Sam Donnelly <samueldonnelly11@gmail.com>
2022-03-20 21:17:10 -0600

    Untracked VS Code configuration files and build files. Updated gitignore to prevent tracking these files.

--- build/HW_125_microSD_driver.d
+++
@@ -1,2 +0,0 @@
-build/HW_125_microSD_driver.o: \
- ../STM32F4-driver-library/sources/HW_125_microSD_driver.c
--- build/mpu6050_driver.d
+++
@@ -1,4 +0,0 @@
-build/mpu6050_driver.o: \
- ../STM32F4-driver-library/sources/mpu6050_driver.c \
- ../STM32F4-driver-library/headers/mpu6050_driver.h
-../STM32F4-driver-library/headers/mpu6050_driver.h:
