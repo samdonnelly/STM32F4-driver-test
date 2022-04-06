39822b9d84a52550dd3fa600a4e24c50f0886b27
Sam Donnelly <samueldonnelly11@gmail.com>
2022-04-05 21:12:28 -0600

    I2C and LCD driver testing.

--- sources/user_app.c
+++ sources/user_app.c
@@ -23,9 +23,19 @@
 // User application 
 void user_app()
 {
-    // Write to terminal 
-    // uart2_sendstring("Hello\r\n");
+    // 
+    static uint8_t data_list[4];
 
     // Delay 
     tim9_delay_ms(100);
+
+    // Test print 
+    data_list[0] = (uint8_t)(0x4D);
+    data_list[1] = (uint8_t)(0x49);
+    data_list[2] = (uint8_t)(0x9D);
+    data_list[3] = (uint8_t)(0x99);
+    i2c1_write_master_mode(
+        data_list, 
+        I2C_4_BYTE, 
+        PCF8574_HHH_WRITE_ADDRESS);
 }
