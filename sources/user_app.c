abe70ad4bdde5a6d5556d7c0986ceb7fab5ec938
Sam Donnelly <samueldonnelly11@gmail.com>
2022-04-06 21:54:41 -0600

    LCD driver testing.

--- sources/user_app.c
+++ sources/user_app.c
@@ -23,19 +23,10 @@
 // User application 
 void user_app()
 {
-    // 
-    static uint8_t data_list[4];
-
     // Delay 
     tim9_delay_ms(1000);
 
     // Test print 
-    data_list[0] = (0x49 & 0xF0) | HD44780U_CONFIG_CMD_0X0D;
-    data_list[1] = (0x49 & 0xF0) | HD44780U_CONFIG_CMD_0X09;
-    data_list[2] = ((0x49 << SHIFT_4) & 0xF0) | HD44780U_CONFIG_CMD_0X0D;
-    data_list[3] = ((0x49 << SHIFT_4) & 0xF0) | HD44780U_CONFIG_CMD_0X09;
-    i2c1_write_master_mode(
-        data_list, 
-        I2C_4_BYTE, 
-        PCF8574_HHH_WRITE_ADDRESS);
+    char string[] = "Hello";
+    hd44780u_send_string(string);
 }
