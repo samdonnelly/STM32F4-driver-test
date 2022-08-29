3d2a437f8dd5a72d14ca315c51fe2b8b6dc7861a
Sam Donnelly <samueldonnelly11@gmail.com>
2022-08-28 19:32:31 -0600

    Added I2C setup code function call to the M8Q driver.

--- sources/test_code/m8q_test.c
+++ sources/test_code/m8q_test.c
@@ -23,12 +23,23 @@
 // Setup code
 void m8q_test_init()
 {
-    // Setup code for the m8q_test here 
+    // I2C communication setup 
+    i2c1_init(
+        I2C1, 
+        I2C1_SDA_PB9,
+        I2C1_SCL_PB8,
+        I2C_MODE_SM,
+        I2C_APB1_42MHZ,
+        I2C_CCR_SM_42_100,
+        I2C_TRISE_1000_42);
+    
+    // SAM-M8Q device setup 
+    // Currently none 
 } 
 
 
 // Test code 
 void m8q_test_app()
 {
-    // Test code for the m8q_test here 
+    // Read 
 }
