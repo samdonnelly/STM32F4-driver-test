16e9d4d500ba316a60fc7ff27569a0542010121f
Sam Donnelly <samueldonnelly11@gmail.com>
2022-10-21 09:24:18 -0600

    Updated function calls to reflect M8Q driver changes. Tested new features of the M8Q driver.

--- sources/test_code/m8q_test.c
+++ sources/test_code/m8q_test.c
@@ -23,7 +23,7 @@
 //=======================================================================================
 // Variables 
 
-uint8_t nmea_msg[150];  // Store NMEA messages 
+// uint8_t nmea_msg[150];  // Store NMEA messages 
 
 //=======================================================================================
 
@@ -80,78 +80,69 @@ void m8q_test_app()
     m8q_user_config(I2C1); 
 #else 
     //===================================================
-    // Low Power mode and TX_READY testing
-    
+    // Data record, power save mode and TX-Ready testing 
+
     // Local variables 
+    uint8_t counter = 0; 
     static uint8_t flipper = 0; 
     static uint16_t timer = 0x48B8; 
     static uint8_t blink = 0; 
 
-    // Display the message if it exists 
-    if (m8q_get_tx_ready())
+    //  M8Q data 
+    uint16_t lat_deg_min = 0; 
+    uint32_t lat_min_frac = 0; 
+    volatile uint8_t NS = 0; 
+    uint16_t lon_deg_min = 0; 
+    uint32_t lon_min_frac = 0; 
+    volatile uint8_t EW = 0; 
+    volatile uint16_t navstat = 0; 
+    uint8_t utc_time[BYTE_9]; 
+    uint8_t utc_date[BYTE_6]; 
+
+    while (TRUE)
     {
-        m8q_read(nmea_msg); 
-        uart_sendstring(USART2, (char *)nmea_msg); 
-        uart_send_new_line(USART2); 
-        blink = GPIO_HIGH - blink; 
-        gpio_write(GPIOA, GPIOX_PIN_5, blink); 
+        if (m8q_get_tx_ready())
+        {
+            // Read the data 
+            m8q_read(); 
+            counter++; 
+
+            // Blink the board LED for visual feedback 
+            blink = GPIO_HIGH - blink; 
+            gpio_write(GPIOA, GPIOX_PIN_5, blink); 
+        }
+        else
+        {
+            if (counter == 2)
+            {
+                m8q_get_lat(&lat_deg_min, &lat_min_frac); 
+                NS = m8q_get_NS(); 
+                m8q_get_long(&lon_deg_min, &lon_min_frac); 
+                EW = m8q_get_EW(); 
+                navstat = m8q_get_navstat(); 
+                m8q_get_time(utc_time); 
+                m8q_get_date(utc_date); 
+                counter = 0; 
+            }
+            break; 
+        }
     }
-    
+
     // Toggle the EXTINT pin 
     if (!(--timer))
     {
         m8q_set_low_power(flipper); 
         flipper = GPIO_HIGH - flipper; 
-        timer = 0x48B8; 
+        timer = 0x68B8; 
         tim9_delay_ms(150);  // Give time for the receiver to startup from sleep mode 
 
         // The following line of code was needed in order for the TX_READY input pin to start 
         // functioning normally again after low power mode. Make this part of a state when 
-        // returning form low power mode. 
-        if (!flipper) while (!(m8q_read(nmea_msg))); 
+        // returning form low power mode.  
+        if (!flipper) while (!(m8q_read())); 
     }
 
-    tim9_delay_ms(1); 
-
-    //===================================================
-
-
-    //===================================================
-    // Data record testing 
-
-    // // Local variables 
-    // uint8_t counter = 0; 
-    // //  M8Q data 
-    // uint16_t lat_deg_min = 0; 
-    // uint32_t lat_min_frac = 0; 
-    // volatile uint8_t NS = 0; 
-    // uint16_t lon_deg_min = 0; 
-    // uint32_t lon_min_frac = 0; 
-    // volatile uint8_t EW = 0; 
-    // volatile uint16_t navstat = 0; 
-    // uint8_t utc_time[BYTE_9]; 
-    // uint8_t utc_date[BYTE_6]; 
-
-    // while (TRUE)
-    // {
-    //     if (m8q_read(I2C1, nmea_msg))
-    //         counter++; 
-    //     else
-    //     {
-    //         if (counter == 2)
-    //         {
-    //             m8q_get_lat(&lat_deg_min, &lat_min_frac); 
-    //             NS = m8q_get_NS(); 
-    //             m8q_get_long(&lon_deg_min, &lon_min_frac); 
-    //             EW = m8q_get_EW(); 
-    //             navstat = m8q_get_navstat(); 
-    //             m8q_get_time(utc_time); 
-    //             m8q_get_date(utc_date); 
-    //             counter = 0; 
-    //         }
-    //         break; 
-    //     }
-    // }
+    tim9_delay_ms(1);
 
     //===================================================
 
