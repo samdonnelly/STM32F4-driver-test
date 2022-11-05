fe5f819503b36f736d7000b346ebed5474d05764
Sam Donnelly <samueldonnelly11@gmail.com>
2022-11-05 16:16:00 -0600

    Added handler function shells to the interrupts handler file.

--- headers/int_handlers.h
+++ headers/int_handlers.h
@@ -23,4 +23,207 @@
 //================================================================================
 
 
+//================================================================================
+// Handlers 
+
+/**
+ * @brief EXTI Line 0 
+ * 
+ * @details 
+ * 
+ */
+void EXTI0_IRQHandler(void); 
+
+
+/**
+ * @brief EXTI Line 1 
+ * 
+ * @details 
+ * 
+ */
+void EXTI1_IRQHandler(void); 
+
+
+/**
+ * @brief EXTI Line 2 
+ * 
+ * @details 
+ * 
+ */
+void EXTI2_IRQHandler(void); 
+
+
+/**
+ * @brief EXTI Line 3 
+ * 
+ * @details 
+ * 
+ */
+void EXTI3_IRQHandler(void); 
+
+
+/**
+ * @brief EXTI Line 4 
+ * 
+ * @details 
+ * 
+ */
+void EXTI4_IRQHandler(void); 
+
+
+/**
+ * @brief DMA1 Stream 0 
+ * 
+ * @details 
+ * 
+ */
+void DMA1_Stream0_IRQHandler(void); 
+
+
+/**
+ * @brief DMA1 Stream 1 
+ * 
+ * @details 
+ * 
+ */
+void DMA1_Stream1_IRQHandler(void); 
+
+
+/**
+ * @brief DMA1 Stream 2 
+ * 
+ * @details 
+ * 
+ */
+void DMA1_Stream2_IRQHandler(void); 
+
+
+/**
+ * @brief DMA1 Stream 3 
+ * 
+ * @details 
+ * 
+ */
+void DMA1_Stream3_IRQHandler(void); 
+
+
+/**
+ * @brief DMA1 Stream 4 
+ * 
+ * @details 
+ * 
+ */
+void DMA1_Stream4_IRQHandler(void); 
+
+
+/**
+ * @brief DMA1 Stream 5 
+ * 
+ * @details 
+ * 
+ */
+void DMA1_Stream5_IRQHandler(void); 
+
+
+/**
+ * @brief DMA1 Stream 6 
+ * 
+ * @details 
+ * 
+ */
+void DMA1_Stream6_IRQHandler(void); 
+
+
+/**
+ * @brief DMA1 Stream 7 
+ * 
+ * @details 
+ * 
+ */
+void DMA1_Stream7_IRQHandler(void); 
+
+
+/**
+ * @brief DMA2 Stream 0 
+ * 
+ * @details 
+ * 
+ */
+void DMA2_Stream0_IRQHandler(void); 
+
+
+/**
+ * @brief DMA2 Stream 1 
+ * 
+ * @details 
+ * 
+ */
+void DMA2_Stream1_IRQHandler(void); 
+
+
+/**
+ * @brief DMA2 Stream 2 
+ * 
+ * @details 
+ * 
+ */
+void DMA2_Stream2_IRQHandler(void); 
+
+
+/**
+ * @brief DMA2 Stream 3 
+ * 
+ * @details 
+ * 
+ */
+void DMA2_Stream3_IRQHandler(void); 
+
+
+/**
+ * @brief DMA2 Stream 4 
+ * 
+ * @details 
+ * 
+ */
+void DMA2_Stream4_IRQHandler(void); 
+
+
+/**
+ * @brief DMA2 Stream 5 
+ * 
+ * @details 
+ * 
+ */
+void DMA2_Stream5_IRQHandler(void); 
+
+
+/**
+ * @brief DMA2 Stream 6 
+ * 
+ * @details 
+ * 
+ */
+void DMA2_Stream6_IRQHandler(void); 
+
+
+/**
+ * @brief DMA2 Stream 7 
+ * 
+ * @details 
+ * 
+ */
+void DMA2_Stream7_IRQHandler(void); 
+
+
+/**
+ * @brief ADC1 
+ * 
+ * @details 
+ * 
+ */
+void ADC_IRQHandler(void); 
+
+//================================================================================
+
+
 #endif   // _INT_HANDLERS_H_ 
--- sources/int_handlers.c
+++ sources/int_handlers.c
@@ -22,4 +22,163 @@
 
 //================================================================================
 // Handlers 
+
+// External interrupt handler names are defined in startup_stm32f411xe.s 
+
+// These handlers are templates for application code. Ensure these are not left with 
+// specific test code in them. 
+
+// EXTI Line 0 
+void EXTI0_IRQHandler(void)
+{
+    // 
+}
+
+
+// EXTI Line 1 
+void EXTI1_IRQHandler(void)
+{
+    // 
+}
+
+
+// EXTI Line 2 
+void EXTI2_IRQHandler(void)
+{
+    // 
+}
+
+
+// EXTI Line 3 
+void EXTI3_IRQHandler(void)
+{
+    // 
+}
+
+
+// EXTI Line 4 
+void EXTI4_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA1 Stream 0 
+void DMA1_Stream0_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA1 Stream 1 
+void DMA1_Stream1_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA1 Stream 2 
+void DMA1_Stream2_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA1 Stream 3 
+void DMA1_Stream3_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA1 Stream 4 
+void DMA1_Stream4_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA1 Stream 5 
+void DMA1_Stream5_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA1 Stream 6 
+void DMA1_Stream6_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA1 Stream 7 
+void DMA1_Stream7_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA2 Stream 0 
+void DMA2_Stream0_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA2 Stream 1 
+void DMA2_Stream1_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA2 Stream 2 
+void DMA2_Stream2_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA2 Stream 3 
+void DMA2_Stream3_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA2 Stream 4 
+void DMA2_Stream4_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA2 Stream 5 
+void DMA2_Stream5_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA2 Stream 6 
+void DMA2_Stream6_IRQHandler(void)
+{
+    // 
+}
+
+
+// DMA2 Stream 7 
+void DMA2_Stream7_IRQHandler(void)
+{
+    // 
+}
+
+
+// ADC1 
+void ADC_IRQHandler(void)
+{
+    // 
+}
+
 //================================================================================
