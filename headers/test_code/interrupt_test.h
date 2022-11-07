dcb9e632da983e3f1f0778a2c2d5caecc97c5d17
Sam Donnelly <samueldonnelly11@gmail.com>
2022-11-06 21:48:16 -0700

    Added template code to the interrupt test code files.

---
+++ headers/test_code/interrupt_test.h
@@ -0,0 +1,44 @@
+/**
+ * @file interrupt_test.h
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief Interrupt test code 
+ * 
+ * @version 0.1
+ * @date 2022-11-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef _INTERRUPT_TEST_H_
+#define _INTERRUPT_TEST_H_
+
+//================================================================================
+// Includes 
+
+#include "includes_drivers.h"
+
+//================================================================================
+
+
+//=======================================================================================
+// Function prototypes 
+
+/**
+ * @brief Interrupt test setup code 
+ * 
+ */
+void int_test_init(void); 
+
+
+/**
+ * @brief Interrupt test application code 
+ * 
+ */
+void int_test_app(void); 
+
+//=======================================================================================
+
+#endif   // _INTERRUPT_TEST_H_
---
+++ sources/test_code/interrupt_test.c
@@ -0,0 +1,67 @@
+/**
+ * @file interrupt_test.c
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief Interrupt test code 
+ * 
+ * @version 0.1
+ * @date 2022-11-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+//================================================================================
+// Includes 
+
+#include "interrupt_test.h" 
+
+//================================================================================
+
+
+// Setup code 
+void int_test_init()
+{
+    // Interrupt initialization code goes here 
+
+    // Initialize timers 
+    tim9_init(TIMERS_APB2_84MHZ_1US_PRESCALAR);
+
+    //==================================================
+    // Timer interrupt enable 
+    //==================================================
+
+    // Initialize GPIO ports 
+    gpio_port_init(); 
+
+    // Initialize UART
+    uart_init(USART2, UART_BAUD_9600, UART_CLOCK_42);
+
+    //==================================================
+    // EXTI init 
+    //==================================================
+
+    //==================================================
+    // ADC Init --> Include ADC interrupts 
+    //================================================== 
+
+    //==================================================
+    // DMA init --> Include DMA interrupts 
+    //==================================================
+}
+
+
+// Test code 
+void int_test_app()
+{
+    // Interrupt application code goes here 
+
+    // Timer interrupt 
+
+    // External interrupt 
+
+    // ADC interrupt 
+
+    // DMA interrupt 
+}
