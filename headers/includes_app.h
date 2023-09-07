af0c7d974cfe4a543d0ee82c5131965685e30542
Sam Donnelly <samueldonnelly11@gmail.com>
2023-09-06 22:03:21 -0600

    Deleted unused test code files.

--- headers/includes_app.h
+++ headers/includes_app.h
@@ -22,21 +22,17 @@
 
 // Test code 
 #include "analog_test.h"
-#include "data_filters_test.h"
 #include "dma_test.h"
-#include "gpio_test.h"
+#include "esc_readytosky_test.h"
 #include "hc05_test.h"
 #include "hd44780u_test.h"
 #include "hw125_test.h"
-#include "i2c_test.h"
 #include "interrupt_test.h"
 #include "lsm303agr_test.h"
 #include "m8q_test.h"
 #include "mpu6050_test.h"
 #include "nrf24l01_test.h"
-#include "spi_test.h"
 #include "timers_test.h"
-#include "uart_test.h"
 #include "ws2812_test.h" 
 
 //=======================================================================================
