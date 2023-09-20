e2797bdbcae78384ae08fb158dafe776da675936
Sam Donnelly <samueldonnelly11@gmail.com>
2023-09-19 22:08:20 -0600

    Started adding test code to the nrf24l01 driver test.

--- headers/test_code/nrf24l01_test.h
+++ headers/test_code/nrf24l01_test.h
@@ -26,17 +26,30 @@
 //=======================================================================================
 // Macros 
 
+// Configuration 
+#define NRF24L01_RF_FREQ 50      // 2400 MHz + this value --> communication frequency (MHz) 
+
 // Conditional compilation 
-#define NRF24L01_MULTI_SPI 0     // Test code using SD card for same SPI bus on multiple pins 
-#define NRF24L01_HEARTBEAT 0     // Heartbeat test code 
 // The test code has code for two RF modules meant for use on separate controllers since 
 // they're made for wireless communication between devices. The following macro ensures only 
 // one controller code or the other is enabled. This can be toggled to easily flash the 
 // correct code to a controller. 
 #define NRF24L01_DEV1_CODE 1     // Device code selection 
+#define NRF24L01_HEARTBEAT 1     // Heartbeat test code 
+#define NRF24L01_MULTI_SPI 0     // Test code using SD card for same SPI bus on multiple pins 
+#define NRF24L01_RC 0            // Remote control test code 
 
-// Configuration 
-#define NRF24L01_RF_FREQ 50      // 2400 MHz + this value --> communication frequency (MHz) 
+// Heartbeat 
+#define NRF24L01_HB_PERIOD 2500000   // Time between heartbeat checks 
+
+// Multi-SPI 
+
+
+// RC 
+#define NRF24L01_LEFT_MOTOR 0x4C   // "L" character that indicates left motor 
+#define NRF24L01_RIGHT_MOTOR 0x52  // "R" character that indicates right motor 
+#define NRF24L01_FWD_THRUST 0x2B   // "+" character that indicates forward thrust 
+#define NRF24L01_REV_THRUST 0x2D   // "-" character that indicates reverse thrust 
 
 //=======================================================================================
 
--- sources/test_code/nrf24l01_test.c
+++ sources/test_code/nrf24l01_test.c
@@ -40,6 +40,41 @@
 
 //=======================================================================================
 // Global variables 
+
+#if NRF24L01_DEV1_CODE   // Start of device 1 code 
+
+#if NRF24L01_HEARTBEAT 
+
+// Device tracker data record 
+typedef struct nrf24l01_test_trackers_s 
+{
+    // Timing information 
+    TIM_TypeDef *timer_nonblocking;             // Timer used for non-blocking delays 
+    tim_compare_t delay_timer;                  // Delay timing info 
+}
+nrf24l01_test_trackers_t; 
+
+// Device tracker instance 
+static nrf24l01_test_trackers_t trackers; 
+
+#elif NRF24L01_MULTI_SPI 
+    // 
+#elif NRF24L01_RC 
+    // 
+#endif   // NRF24L01_HEARTBEAT || NRF24L01_MULTI_SPI || NRF24L01_RC 
+
+#else   // NRF24L01_DEV1_CODE --> End of device 1 code, start of device 2 code 
+    // 
+#if NRF24L01_HEARTBEAT 
+    // 
+#elif NRF24L01_MULTI_SPI 
+    // 
+#elif NRF24L01_RC 
+    // 
+#endif   // NRF24L01_HEARTBEAT || NRF24L01_MULTI_SPI || NRF24L01_RC 
+
+#endif   // NRF24L01_DEV1_CODE --> End of device 2 code 
+
 //=======================================================================================
 
 
@@ -79,15 +114,15 @@ void nrf24l01_test_init(void)
     // SPI for the RF module 
     spi_init(
         SPI2, 
-        GPIOB,   // SCK pin GPIO port 
+        GPIOB,   // GPIO port for SCK pin 
         PIN_10,  // SCK pin 
-        GPIOC,   // Data (MISO/MOSI) pin GPIO port 
+        GPIOC,   // GPIO port for data (MISO/MOSI) pins 
         PIN_2,   // MISO pin 
         PIN_3,   // MOSI pin 
         SPI_BR_FPCLK_16, 
         SPI_CLOCK_MODE_0); 
 
-#if NRF24L01_MULTI_SPI && NRF24L01_DEV1_CODE 
+#if NRF24L01_MULTI_SPI && !NRF24L01_DEV1_CODE 
 
     // SPI2 and slave select pin for SD card 
     // This is on different pins than the RF module to test if the same SPI bus works across 
@@ -125,13 +160,47 @@ void nrf24l01_test_init(void)
     //===================================================
     // Initialize SD card --> for testing multiple SPI pins on the same SPI bus 
 
-#if NRF24L01_MULTI_SPI && NRF24L01_DEV1_CODE 
+#if NRF24L01_MULTI_SPI && !NRF24L01_DEV1_CODE 
 
     // SD card user initialization 
     hw125_user_init(SPI2, GPIOB, GPIOX_PIN_12); 
 
 #endif   // NRF24L01_MULTI_SPI 
     
+    //===================================================
+
+    //===================================================
+    // Variable initialization 
+
+#if NRF24L01_DEV1_CODE   // Start of device 1 code 
+
+#if NRF24L01_HEARTBEAT 
+
+    // Timing information 
+    trackers.timer_nonblocking = TIM9; 
+    trackers.delay_timer.clk_freq = tim_get_pclk_freq(trackers.timer_nonblocking); 
+    trackers.delay_timer.time_cnt_total = CLEAR; 
+    trackers.delay_timer.time_cnt = CLEAR; 
+    trackers.delay_timer.time_start = SET_BIT; 
+
+#elif NRF24L01_MULTI_SPI 
+    // 
+#elif NRF24L01_RC 
+    // 
+#endif   // NRF24L01_HEARTBEAT || NRF24L01_MULTI_SPI || NRF24L01_RC 
+
+#else   // NRF24L01_DEV1_CODE --> End of device 1 code, start of device 2 code 
+    // 
+#if NRF24L01_HEARTBEAT 
+    // 
+#elif NRF24L01_MULTI_SPI 
+    // 
+#elif NRF24L01_RC 
+    // 
+#endif   // NRF24L01_HEARTBEAT || NRF24L01_MULTI_SPI || NRF24L01_RC 
+
+#endif   // NRF24L01_DEV1_CODE --> End of device 2 code 
+
     //===================================================
 }
 
@@ -141,16 +210,58 @@ void nrf24l01_test_init(void)
 //=======================================================================================
 // Test code 
 
+// Heartbeat 
+// - Device 1 (master) sends a 'ping' to device 2 (slave) periodically 
+// - Device 2 reads ping and responds with a confirmation 
+// - Device 1 reads confirmation to verify communication is working 
+// - If device 1 doesn't get a response then indicate a loss of communication 
+// - Option to enter a "configuration" mode (on both devices) where settings can be 
+//   manually changed by the user. Can then switch back to heartbeat mode to make sure 
+//   there is still communication. 
+
+// Multi-SPI 
+// - Device 1 (master) sends a 'ping' to device 2 (slave) upon user request 
+// - Device 2 reads ping and attempts to save an acknowledgment to an SD card 
+// - Device 2 reports back the status of the SD card write to device 1 
+
+// RC 
+// - Device 1 (master) sends throttle command to device 2 (slave) 
+// - Device 2 reads throttle command 
+// - Throttle command goes to the ESC driver 
+
 void nrf24l01_test_app(void)
 {
 #if NRF24L01_DEV1_CODE   // Start of device 1 code 
+
+#if NRF24L01_HEARTBEAT 
+
+    // Periodically ping the second device 
+    if (tim_compare(trackers.timer_nonblocking, 
+                    trackers.delay_timer.clk_freq, 
+                    NRF24L01_HB_PERIOD, 
+                    &trackers.delay_timer.time_cnt_total, 
+                    &trackers.delay_timer.time_cnt, 
+                    &trackers.delay_timer.time_start))
+    {
+        // trackers.delay_timer.time_start = SET_BIT; 
+    }
+
+#elif NRF24L01_MULTI_SPI 
     // 
-#if NRF24L01_MULTI_SPI 
+#elif NRF24L01_RC 
     // 
-#endif   // NRF24L01_MULTI_SPI 
+#endif   // NRF24L01_HEARTBEAT || NRF24L01_MULTI_SPI || NRF24L01_RC 
 
 #else   // NRF24L01_DEV1_CODE --> End of device 1 code, start of device 2 code 
     // 
+#if NRF24L01_HEARTBEAT 
+    // 
+#elif NRF24L01_MULTI_SPI 
+    // 
+#elif NRF24L01_RC 
+    // 
+#endif   // NRF24L01_HEARTBEAT || NRF24L01_MULTI_SPI || NRF24L01_RC 
+
 #endif   // NRF24L01_DEV1_CODE --> End of device 2 code 
 }
 
