67675440f7f56f5dba270b293a4b015f3f3f9caf
Sam <samueldonnelly11@gmail.com>
2024-03-22 17:29:07 -0600

    active object testing.

--- sources/app_test/active_object_test.cpp
+++ sources/app_test/active_object_test.cpp
@@ -45,83 +45,238 @@
 
 
 //=======================================================================================
-// Variables 
+// Active Object module data 
 
-// Thread handles 
-static osThreadId_t ThreadLowHandle = nullptr; 
-static osThreadId_t ThreadHighHandle = nullptr; 
+//==================================================
+// Datatype 
 
-// Thread definition: Low Priority 
-const static osThreadAttr_t thread_low_attributes = 
+typedef uint8_t Event; 
+
+//==================================================
+
+//==================================================
+// Objects 
+
+// Thread Event Info 
+typedef struct 
 {
-    .name = "ThreadLow", 
-    .attr_bits = CLEAR, 
-    .cb_mem = NULL, 
-    .cb_size = CLEAR, 
-    .stack_mem = NULL, 
-    .stack_size = THREAD_LOW_STACK_SIZE, 
-    .priority = (osPriority_t)osPriorityLow, 
-    .tz_module = CLEAR, 
-    .reserved = CLEAR 
-}; 
+    osThreadAttr_t attr;              // Thread attributes 
+    uint8_t event;                    // Event index 
+    QueueHandle_t ThreadEventQueue;   // Queue 
+    void (*dispatch)(Event event);    // Dispatch function 
+}
+ThreadEventData; 
 
-// Thread definition: High Priority 
-const static osThreadAttr_t thread_high_attributes = 
-{
-    .name = "ThreadHigh", 
-    .attr_bits = CLEAR, 
-    .cb_mem = NULL, 
-    .cb_size = CLEAR, 
-    .stack_mem = NULL, 
-    .stack_size = THREAD_HIGH_STACK_SIZE, 
-    .priority = (osPriority_t)osPriorityHigh, 
-    .tz_module = CLEAR, 
-    .reserved = CLEAR 
-}; 
+//==================================================
 
-
-// Queue info 
-static const uint8_t queue_len = 3; 
-static QueueHandle_t ThreadLowEventQueue; 
-static QueueHandle_t ThreadHighEventQueue; 
-
-//=======================================================================================
-
-
-//=======================================================================================
+//==================================================
 // Prototypes 
 
 /**
  * @brief Common event loop shared by all threads 
  * 
- * @param argument : 
+ * @param thread_info : thread info such as the event queue handler and dispatch function 
  */
-void eventLoop(void *argument); 
+void eventLoop(void *thread_info); 
 
+//==================================================
+
+//=======================================================================================
+
+
+//=======================================================================================
+// Low Priority Thread module data 
+
+// Local data to the low priority thread 
+
+//==================================================
+// Enums 
+
+// Low Priority Thread states 
+typedef enum {
+    THREAD_LOW_STATE_0, 
+    THREAD_LOW_STATE_1, 
+    THREAD_LOW_NUM_STATES 
+} ThreadLowStates; 
+
+// Low Priority Thread event indexes 
+typedef enum {
+    THREAD_LOW_NO_EVENT, 
+    THREAD_LOW_EVENT_0, 
+    THREAD_LOW_EVENT_1, 
+    THREAD_LOW_EVENT_2, 
+} ThreadLowEvents; 
+
+//==================================================
+
+//==================================================
+// Objects 
+
+// Low Priority Thread trackers 
+struct ThreadLowTrackers 
+{
+    ThreadEventData event_data; 
+    ThreadLowStates state; 
+}; 
+
+// Low Priority Thread object 
+static ThreadLowTrackers thread_low_trackers; 
+
+//==================================================
+
+//==================================================
+// Variables 
+
+// Queue info 
+static const uint8_t thread_low_queue_len = 3; 
+
+//==================================================
+
+//==================================================
+// Function pointers 
+
+// State function pointers 
+typedef void (*thread_low_state_ptr)(ThreadLowTrackers *trackers, Event event); 
+
+//==================================================
+
+//==================================================
+// Prototypes 
+
+// Setup 
+void ThreadLowSetup(void); 
 
 // Dispatch functions 
-void DispatchThreadLow(void); 
-void DispatchThreadHigh(void); 
+void DispatchThreadLow(Event event); 
 
 // State functions 
-void ThreadLowState0(void); 
-void ThreadLowState1(void); 
-void ThreadHighState0(void); 
-void ThreadHighState1(void); 
+void ThreadLowState0(ThreadLowTrackers *trackers, Event event); 
+void ThreadLowState1(ThreadLowTrackers *trackers, Event event); 
+
+//==================================================
+
+//==================================================
+// Control data 
+
+// Low Priority Thread state table 
+static const thread_low_state_ptr thread_low_state_table[THREAD_LOW_NUM_STATES] = 
+{
+    &ThreadLowState0,   // State 0 
+    &ThreadLowState1    // State 1 
+}; 
+
+//==================================================
+
+//=======================================================================================
+
+
+//=======================================================================================
+// High Priority Thread module data 
+
+// Local data to the high priority thread 
+
+//==================================================
+// Enums 
+
+// High Priority thread states 
+typedef enum {
+    THREAD_HIGH_STATE_0, 
+    THREAD_HIGH_STATE_1, 
+    THREAD_HIGH_NUM_STATES 
+} ThreadHighStates; 
+
+// High Priority Thread event indexes 
+typedef enum {
+    THREAD_HIGH_NO_EVENT, 
+    THREAD_HIGH_EVENT_0, 
+    THREAD_HIGH_EVENT_1, 
+    THREAD_HIGH_EVENT_2, 
+} ThreadHighEvents; 
+
+//==================================================
+
+//==================================================
+// Objects 
+
+// High Priority Thread trackers 
+struct ThreadHighTrackers 
+{
+    ThreadEventData event_data; 
+    ThreadHighStates state; 
+}; 
+
+// High Priority Thread object 
+static ThreadHighTrackers thread_high_trackers; 
+
+//==================================================
+
+//==================================================
+// Variables 
+
+// Queue info 
+static const uint8_t thread_high_queue_len = 3; 
+
+//==================================================
+
+//==================================================
+// Function pointers 
+
+// State function pointers 
+typedef void (*thread_high_state_ptr)(ThreadHighTrackers *trackers, Event event); 
+
+//==================================================
+
+//==================================================
+// Prototypes 
+
+// Setup 
+void ThreadHighSetup(void); 
+
+// Dispatch functions 
+void DispatchThreadHigh(Event event); 
+
+// State functions 
+void ThreadHighState0(ThreadHighTrackers *trackers, Event event); 
+void ThreadHighState1(ThreadHighTrackers *trackers, Event event); 
+
+//==================================================
+
+//==================================================
+// Control data 
+
+// High Priority Thread state table 
+static const thread_high_state_ptr thread_high_state_table[THREAD_HIGH_NUM_STATES] = 
+{
+    &ThreadHighState0,   // State 0 
+    &ThreadHighState1    // State 1 
+}; 
+
+//==================================================
+
+//=======================================================================================
+
+
+//=======================================================================================
+// Event module(s) 
+
+//==================================================
+// Prototypes 
 
 // Events 
-void ThreadLowEvent0(void); 
-void ThreadLowEvent1(void); 
-void ThreadLowEvent2(void); 
-void ThreadHighEvent0(void); 
-void ThreadHighEvent1(void);
-void ThreadHighEvent2(void); 
+void Event0(void); 
+void Event1(void); 
+void Event2(void); 
+void Event3(void); 
+void Event4(void);
+void Event5(void); 
+
+//==================================================
 
 //=======================================================================================
 
 
 //=======================================================================================
-// Setup 
+// Test functions 
 
 // Active Object test setup code 
 void active_object_test_init(void)
@@ -129,20 +284,12 @@ void active_object_test_init(void)
     // Initialize FreeRTOS scheduler 
     osKernelInitialize(); 
 
-    // Create the thread(s) 
-    ThreadLowHandle = osThreadNew(eventLoop, NULL, &thread_low_attributes); 
-    ThreadHighHandle = osThreadNew(eventLoop, NULL, &thread_high_attributes); 
+    // Other general setup (pins, ports, etc.) 
 
-    // Create queue handle(s) 
-    ThreadLowEventQueue = xQueueCreate(queue_len, sizeof(uint32_t)); 
-    ThreadHighEventQueue = xQueueCreate(queue_len, sizeof(uint32_t)); 
+    ThreadLowSetup(); 
+    ThreadHighSetup(); 
 }
 
-//=======================================================================================
-
-
-//=======================================================================================
-// Test code 
 
 // Active Object test code 
 void active_object_test_app(void)
@@ -151,27 +298,27 @@ void active_object_test_app(void)
     osKernelStart(); 
 }
 
-#if AO_C_TEST 
-#elif AO_CPP_TEST 
-#endif 
-
 //=======================================================================================
 
 
 //=======================================================================================
-// Event loop 
+// Active Object control 
+
+// In practice the event loop and any other context switching or thread related code 
+// should be in it's own file. The event loop would then dispatch to the thread it's 
+// running in a different file. 
 
 // Common event loop shared by all threads 
-void eventLoop(void *argument)
+void eventLoop(void *thread_info)
 {
-    // 
+    ThreadEventData *thread = (ThreadEventData *)thread_info; 
 
     // Event loop 
     while (1)
     {
-        // Queue 
-        // xQueueReceive(queue, (void *)&event, portMAX_DELAY); 
-        // Dispatch 
+        thread->event = CLEAR; 
+        xQueueReceive(thread->ThreadEventQueue, (void *)&thread->event, portMAX_DELAY); 
+        thread->dispatch(thread->event); 
     }
 
     vTaskDelete(NULL); 
@@ -181,103 +328,298 @@ void eventLoop(void *argument)
 
 
 //=======================================================================================
-// Dispatch functions 
+// Low Priority Thread event control 
+
+// In practice these dispatch functions would likely be separated into different files 
+// to help organize the code. 
+
+// Setup 
+void ThreadLowSetup(void)
+{
+    // Thread definition, queue handle creation and dispatch function assignment 
+    thread_low_trackers.event_data = 
+    {
+        .attr = { .name = "ThreadLow", 
+                  .attr_bits = CLEAR, 
+                  .cb_mem = NULL, 
+                  .cb_size = CLEAR, 
+                  .stack_mem = NULL, 
+                  .stack_size = THREAD_LOW_STACK_SIZE, 
+                  .priority = (osPriority_t)osPriorityLow, 
+                  .tz_module = CLEAR, 
+                  .reserved = CLEAR }, 
+        .event = CLEAR, 
+        .ThreadEventQueue = xQueueCreate(thread_low_queue_len, sizeof(uint32_t)), 
+        .dispatch = DispatchThreadLow 
+    }; 
+    // Check that the queues were created successfully 
+
+    // Create the thread(s) 
+    osThreadNew(
+        eventLoop, 
+        (void *)&thread_low_trackers.event_data, 
+        &thread_low_trackers.event_data.attr); 
+    // Check that the thread creation worked 
+}
+
 
 // Dispatch function for low priority task 
-void DispatchThreadLow(void)
+void DispatchThreadLow(Event event)
 {
+    ThreadLowStates state = thread_low_trackers.state; 
+
+    // Continuous events 
+
     // State machine 
+    switch (state)
+    {
+        case THREAD_LOW_STATE_0: 
+            state = THREAD_LOW_STATE_1; 
+            break; 
+
+        case THREAD_LOW_STATE_1: 
+            state = THREAD_LOW_STATE_0; 
+            break; 
+        
+        default: 
+            state = THREAD_LOW_STATE_0; 
+            break; 
+    }
 
     // if (xQueueSend(queue, (void *)&e, 0) == pdTRUE); 
+
+    // Run state function 
+    thread_low_state_table[state](&thread_low_trackers, event); 
+    thread_low_trackers.state = state; 
 }
 
 
-// Dispatch function for high priority thread 
-void DispatchThreadHigh(void)
-{
-    // State machine 
-}
-
-//=======================================================================================
-
-
-//=======================================================================================
-// State functions 
+// In practice the state functions would likely be in the same file as the function that 
+// dispatched them. Each dispatching function would likely have its own file. 
 
 // Low Priority Thread: State 0 
-void ThreadLowState0(void)
+void ThreadLowState0(
+    ThreadLowTrackers *trackers, 
+    Event event)
 {
-    // Event switch statement 
+    // State entry 
+
+    // Event selection 
+    switch (event)
+    {
+        case THREAD_LOW_EVENT_0: 
+            Event0(); 
+            break; 
+
+        case THREAD_LOW_EVENT_1: 
+            Event1(); 
+            break; 
+
+        default: 
+            break; 
+    }
+
+    // State exit 
 }
 
 
 // Low Priority Thread: State 1 
-void ThreadLowState1(void)
+void ThreadLowState1(
+    ThreadLowTrackers *trackers, 
+    Event event)
 {
-    // Event switch statement 
+    // State entry 
+
+    // Event selection 
+    switch (event)
+    {
+        case THREAD_LOW_EVENT_0: 
+            Event0(); 
+            break; 
+
+        case THREAD_LOW_EVENT_2: 
+            Event2(); 
+            break; 
+
+        default: 
+            break; 
+    }
+
+    // State exit 
+}
+
+//=======================================================================================
+
+
+//=======================================================================================
+// High Priority Thread event control 
+
+// In practice these dispatch functions would likely be separated into different files 
+// to help organize the code. 
+
+// Setup 
+void ThreadHighSetup(void)
+{
+    // Thread definition, queue handle creation and dispatch function assignment 
+    thread_low_trackers.event_data = 
+    {
+        .attr = { .name = "ThreadHigh", 
+                  .attr_bits = CLEAR, 
+                  .cb_mem = NULL, 
+                  .cb_size = CLEAR, 
+                  .stack_mem = NULL, 
+                  .stack_size = THREAD_HIGH_STACK_SIZE, 
+                  .priority = (osPriority_t)osPriorityHigh, 
+                  .tz_module = CLEAR, 
+                  .reserved = CLEAR }, 
+        .event = CLEAR, 
+        .ThreadEventQueue = xQueueCreate(thread_high_queue_len, sizeof(uint32_t)), 
+        .dispatch = DispatchThreadHigh 
+    }; 
+    // Check that the queues were created successfully 
+
+    // Create the thread(s) 
+    osThreadNew(
+        eventLoop, 
+        (void *)&thread_low_trackers.event_data, 
+        &thread_low_trackers.event_data.attr); 
+    // Check that the thread creation worked 
 }
 
 
-// High Priority Thread: State 0 
-void ThreadHighState0(void)
+// Dispatch function for high priority thread 
+void DispatchThreadHigh(Event event)
 {
-    // Event switch statement 
+    // A thread does not necessarily need a state machine. Instead, there could simply be 
+    // a list of events available for when they're requested. For example, the high 
+    // priority thread could be dedicated to reading data from devices irrespective of 
+    // state and each read gets triggered by a timer. 
+
+    ThreadHighStates state = thread_high_trackers.state; 
+
+    // Continuous events 
+
+    // State machine 
+    switch (state)
+    {
+        case THREAD_HIGH_STATE_0: 
+            state = THREAD_HIGH_STATE_1; 
+            break; 
+
+        case THREAD_HIGH_STATE_1: 
+            state = THREAD_HIGH_STATE_0; 
+            break; 
+        
+        default: 
+            state = THREAD_HIGH_STATE_0; 
+            break; 
+    }
+
+    // Run state function 
+    thread_high_state_table[state](&thread_high_trackers, event); 
+    thread_high_trackers.state = state; 
+}
+
+
+// In practice the state functions would likely be in the same file as the function that 
+// dispatched them. Each dispatching function would likely have its own file. 
+
+// High Priority Thread: State 0 
+void ThreadHighState0(
+    ThreadHighTrackers *trackers, 
+    Event event)
+{
+    // State entry 
+
+    // Event selection 
+    switch (event)
+    {
+        case THREAD_HIGH_EVENT_0: 
+            Event3(); 
+            break; 
+
+        case THREAD_HIGH_EVENT_1: 
+            Event4(); 
+            break; 
+
+        default: 
+            break; 
+    }
+
+    // State exit 
 }
 
 
 // High Priority Thread: State 1 
-void ThreadHighState1(void)
+void ThreadHighState1(
+    ThreadHighTrackers *trackers, 
+    Event event)
 {
-    // Event switch statement 
-}
+    // State entry 
 
+    // Event selection 
+    switch (event)
+    {
+        case THREAD_HIGH_EVENT_0: 
+            Event3(); 
+            break; 
+
+        case THREAD_HIGH_EVENT_2: 
+            Event5(); 
+            break; 
+
+        default: 
+            break; 
+    }
+
+    // State exit 
+}
 
 //=======================================================================================
 
 
 //=======================================================================================
-// Event functions 
+// Event modules 
 
-// In practice these would each be an independent module that can be used wherever 
-// needed. 
+// In practice these would be independent modules that can be used wherever needed. 
 
-// Low Priority Thread: Event 0 
-void ThreadLowEvent0(void)
+// Event 0 
+void Event0(void)
 {
     // 
 }
 
 
-// Low Priority Thread: Event 1 
-void ThreadLowEvent1(void)
+// Event 1 
+void Event1(void)
 {
     // 
 }
 
 
-// Low Priority Thread: Event 2 
-void ThreadLowEvent2(void)
+// Event 2 
+void Event2(void)
 {
     // 
 }
 
 
-// High Priority Thread: Event 0 
-void ThreadHighEvent0(void)
+// Event 3 
+void Event3(void)
 {
     // 
 }
 
 
-// High Priority Thread: Event 1 
-void ThreadHighEvent1(void)
+// Event 4 
+void Event4(void)
 {
     // 
 }
 
 
-// High Priority Thread: Event 2 
-void ThreadHighEvent2(void)
+// Event 5 
+void Event5(void)
 {
     // 
 }
