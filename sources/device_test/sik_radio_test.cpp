ca6bb0761abb8933a47eaf4d63d2014db33c85a7
Sam Donnelly <samueldonnelly11@gmail.com>
2024-12-14 22:35:50 -0700

    Added UART init code to the SiK radio driver test.

--- sources/device_test/sik_radio_test.cpp
+++ sources/device_test/sik_radio_test.cpp
@@ -57,9 +57,36 @@ static mavlink_data_t mavlink_data;
 
 void sik_radio_test_init(void)
 {
+    // Initialize data 
     mavlink_data.channel = MAVLINK_COMM_0; 
     memset((void *)mavlink_data.msg_buff, CLEAR, SIK_TEST_MSG_BUFF_SIZE); 
     mavlink_data.msg_buff_index = CLEAR; 
+
+    // UART1 - SiK radio module 
+    uart_init(
+        USART1, 
+        GPIOA, 
+        PIN_10, 
+        PIN_9, 
+        UART_FRAC_84_115200, 
+        UART_MANT_84_115200, 
+        UART_DMA_DISABLE, 
+        UART_DMA_DISABLE); 
+
+    // UART2 - Serial terminal 
+    uart_init(
+        USART2, 
+        GPIOA, 
+        PIN_3, 
+        PIN_2, 
+        UART_FRAC_42_9600, 
+        UART_MANT_42_9600, 
+        UART_DMA_DISABLE, 
+        UART_DMA_ENABLE);   // DMA enabled so it can be configured later 
+
+    // DMA - UART1 
+
+    // DMA - UART2 
 }
 
 //=======================================================================================
