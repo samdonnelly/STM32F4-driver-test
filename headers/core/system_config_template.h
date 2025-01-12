fb9396f73e5b489ebaf636818a6cd93c51ab90ee
Sam Donnelly <samueldonnelly11@gmail.com>
2025-01-11 21:01:06 -0700

    Moved the timers driver test conditional macros to the system config. Added a compiler warning exclusion for the mavlink library.

--- headers/core/system_config_template.h
+++ headers/core/system_config_template.h
@@ -239,7 +239,21 @@
 
 //==================================================
 
+//==================================================
 // Timers 
+
+#define TIM_PERIODIC 0                   // Periodic interrupt code 
+#define TIM_PERIODIC_COUNT 0             // Periodic interrupt counter printout code 
+#define TIM_PWM_OUTPUT 0                 // PWM output code (requires periodic interrupt code) 
+#define TIM_WS2812 0                     // WS2812 device code 
+#define TIM_SWITCH_1 0                   // User switch 1 (requires periodic interrupt code) 
+#define TIM_SWITCH_2 0                   // User switch 2 (requires user switch 0 code) 
+#define TIM_SWITCH_3 0                   // User switch 3 (requires user switch 2 code) 
+#define TIM_SWITCH_4 0                   // User switch 4 (requires user switch 3 code) 
+#define TIM_CLK_FREQ 0                   // Code for checking system clock frequencies 
+#define TIM_NON_BLOCKING 0               // Non-blocking delay code 
+
+//==================================================
 
 //=======================================================================================
 
