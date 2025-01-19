c106a8f6e1c4c8b6d6ad12ca21e0f5d85583a066
Sam <samueldonnelly11@gmail.com>
2025-01-18 17:31:14 -0700

    Started rewriting the UART driver test.

--- headers/peripheral_test/uart_test.h
+++ headers/peripheral_test/uart_test.h
@@ -3,7 +3,7 @@
  * 
  * @author Sam Donnelly (samueldonnelly11@gmail.com)
  * 
- * @brief UART test code interface 
+ * @brief UART test interface 
  * 
  * @version 0.1
  * @date 2023-09-20
--- sources/peripheral_test/uart_test.c
+++ sources/peripheral_test/uart_test.c
@@ -3,7 +3,25 @@
  * 
  * @author Sam Donnelly (samueldonnelly11@gmail.com)
  * 
- * @brief UART test code 
+ * @brief UART test 
+ * 
+ * @details Setup 
+ *          - Hardware 
+ *            * STM32F4 microcontroller with a serial connection to a PC 
+ *          - Software 
+ *            * Serial monitor on a PC to allow the exchange of info with the STM32F4. 
+ *          
+ *          Configuration 
+ *          - UART 
+ *            * One UART port must be configured for the serial terminal connection. 
+ *          
+ *          Dependencies 
+ *          - STM32F4 driver library 
+ *            * UART drivers are used along with some functions for circular buffer 
+ *              handling. 
+ *          
+ *          Procedure 
+ *          - 
  * 
  * @version 0.1
  * @date 2023-09-20
@@ -68,9 +86,6 @@ void uart_test_init(void)
     // Initialize GPIO ports 
     gpio_port_init(); 
 
-    //==================================================
-    // UART initialization 
-
     // Initialize UART 
     uart_init(
         USART2, 
@@ -80,68 +95,9 @@ void uart_test_init(void)
         UART_FRAC_42_9600, 
         UART_MANT_42_9600, 
         UART_DMA_DISABLE, 
-        UART_DMA_ENABLE); 
-    
-    // Enable IDLE line interrupts - this interrupt is triggered when the RX line 
-    // goes idle (no more data coming in). This is used to read variable length 
-    // UART inputs via DMA. If DMA transfer interrupts are used then the interrupt 
-    // will only trigger for fixed lengths. 
-    uart_interrupt_init(
-        USART2, 
-        UART_INT_DISABLE, 
-        UART_INT_DISABLE, 
-        UART_INT_DISABLE, 
-        UART_INT_DISABLE, 
-        UART_INT_ENABLE, 
-        UART_INT_DISABLE, 
-        UART_INT_DISABLE); 
+        UART_DMA_DISABLE); 
 
-    //==================================================
-
-    //===================================================
-    // Initialize DMA 
-
-    // Initialize the DMA stream 
-    dma_stream_init(
-        DMA1, 
-        DMA1_Stream5, 
-        DMA_CHNL_4, 
-        DMA_DIR_PM, 
-        DMA_CM_ENABLE,
-        DMA_PRIOR_VHI, 
-        DMA_DBM_DISABLE, 
-        DMA_ADDR_INCREMENT,   // Increment the buffer pointer to fill the buffer 
-        DMA_ADDR_FIXED,       // No peripheral increment - copy from DR only 
-        DMA_DATA_SIZE_BYTE, 
-        DMA_DATA_SIZE_BYTE); 
-
-    // Configure the DMA stream 
-    dma_stream_config(
-        DMA1_Stream5, 
-        (uint32_t)(&USART2->DR), 
-        (uint32_t)cb.cb, 
-        (uint32_t)NULL, 
-        (uint16_t)UART_TEST_MAX_INPUT); 
-
-    // Enable the DMA stream 
-    dma_stream_enable(DMA1_Stream5); 
-    
-    //===================================================
-
-    //==================================================
-    // Initialize interrupts 
-
-    // Initialize interrupt handler flags (called once) 
-    int_handler_init(); 
-
-    // Enable the interrupt handlers (called for each interrupt) - for USART2_RX 
-    nvic_config(USART2_IRQn, EXTI_PRIORITY_0); 
-
-    //==================================================
-
-    //==================================================
     // Initialize data 
-
     cb.uart = USART2; 
     cb.dma_stream = DMA1_Stream5; 
     memset((void *)cb.cb, CLEAR, sizeof(cb.cb)); 
@@ -153,8 +109,7 @@ void uart_test_init(void)
     cb.dma_index.ndt_new = CLEAR; 
     memset((void *)cb.data_buff, CLEAR, sizeof(cb.data_buff)); 
 
-    //==================================================
-
+    // Provide an initial prompt to the user 
     uart_test_user_prompt(); 
 } 
 
@@ -166,23 +121,8 @@ void uart_test_init(void)
 
 void uart_test_app(void)
 {
-    // When an idle line interrupt occurs (i.e. UART RX line goes idle for too long) 
-    // indicating the end of the received serial terminal data, the data will be parsed 
-    // and echoed back to the serial terminal to verify that it has been parsed 
-    // correctly. The number of data items transferred by DMA is first found before 
-    // the circular buffer can be parsed. 
-    if (handler_flags.usart2_flag)
-    {
-        handler_flags.usart2_flag = CLEAR; 
-        
-        dma_cb_index(cb.dma_stream, &cb.dma_index, &cb.cb_index); 
-        cb_parse(cb.cb, &cb.cb_index, cb.data_buff); 
-
-        uart_send_new_line(cb.uart); 
-        uart_send_str(cb.uart, (char *)cb.data_buff); 
-        uart_send_new_line(cb.uart); 
-        uart_test_user_prompt(); 
-    }
+    // Look for data 
+    uart_get_data(cb.uart, cb.data_buff); 
 }
 
 //=======================================================================================
