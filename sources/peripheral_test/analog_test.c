5b1abf0b4c45b04d9a1a7a9d550ecf1cb7156060
Sam Donnelly <samueldonnelly11@gmail.com>
2025-01-25 22:44:57 -0700

    Updated comments and formatting in the analog driver test.

--- sources/peripheral_test/analog_test.c
+++ sources/peripheral_test/analog_test.c
@@ -3,7 +3,27 @@
  * 
  * @author Sam Donnelly (samueldonnelly11@gmail.com)
  * 
- * @brief Analog test code 
+ * @brief Analog test 
+ * 
+ * @details Setup 
+ *          - Hardware 
+ *            * STM32F4 microcontroller with a serial connection to a PC 
+ *          
+ *          Configuration 
+ *          - UART 
+ *            * 
+ *          - ADC 
+ *            * 
+ *          
+ *          Dependencies 
+ *          - STM32F4 driver library 
+ *            - 
+ *          
+ *          Procedure 
+ *          - 
+ *          
+ *          Other tests demonstrating the analog driver: 
+ *          - dma_test.c 
  * 
  * @version 0.1
  * @date 2022-08-28
@@ -21,24 +41,60 @@
 
 
 //=======================================================================================
+// Macros 
+
+#define ANALOG_TEST_NUM_ADC 2 
+#define ANALOG_TEST_DELAY_TIME 1000 
+
+//=======================================================================================
+
+
+//=======================================================================================
+// Global variables 
+
+typedef struct analog_test_data_s 
+{
+    USART_TypeDef *uart; 
+    ADC_TypeDef *adc; 
+    TIM_TypeDef *timer; 
+    uint16_t adc_data[ANALOG_TEST_NUM_ADC]; 
+    adc_channel_t adc_channel[ANALOG_TEST_NUM_ADC]; 
+
+}
+analog_test_data_t; 
+
+static analog_test_data_t analog_data; 
+
+//=======================================================================================
+
+
+//=======================================================================================
 // Setup code
 
 void analog_test_init()
 {
-    // Initialize timers 
-    tim_9_to_11_counter_init(
-        TIM9, 
-        TIM_84MHZ_1US_PSC, 
-        0xFFFF,  // Max ARR value 
-        TIM_UP_INT_DISABLE); 
-    tim_enable(TIM9); 
+    // Initialize data 
+    analog_data.uart = USART2; 
+    analog_data.adc = ADC1; 
+    analog_data.timer = TIM9; 
+    memset((void *)analog_data.adc_data, CLEAR, sizeof(analog_data.adc_data)); 
+    analog_data.adc_channel[0] = ADC_CHANNEL_10; 
+    analog_data.adc_channel[1] = ADC_CHANNEL_11; 
 
     // Initialize GPIO ports 
     gpio_port_init(); 
 
+    // Initialize timers 
+    tim_9_to_11_counter_init(
+        analog_data.timer, 
+        TIM_84MHZ_1US_PSC, 
+        0xFFFF,  // Max ARR value 
+        TIM_UP_INT_DISABLE); 
+    tim_enable(analog_data.timer); 
+
     // Initialize UART
     uart_init(
-        USART2, 
+        analog_data.uart, 
         GPIOA, 
         PIN_3, 
         PIN_2, 
@@ -53,7 +109,7 @@ void analog_test_init()
     // Initialize the ADC port (called once) 
     adc1_clock_enable(RCC); 
     adc_port_init(
-        ADC1, 
+        analog_data.adc, 
         ADC1_COMMON, 
         ADC_PCLK2_4, 
         ADC_RES_8, 
@@ -66,20 +122,20 @@ void analog_test_init()
         ADC_PARAM_DISABLE); 
 
     // Initialize ADC pins and channels (called for each pin/channel) 
-    adc_pin_init(ADC1, GPIOC, PIN_0, ADC_CHANNEL_10, ADC_SMP_15); 
-    adc_pin_init(ADC1, GPIOC, PIN_1, ADC_CHANNEL_11, ADC_SMP_15); 
+    adc_pin_init(analog_data.adc, GPIOC, PIN_0, analog_data.adc_channel[0], ADC_SMP_15); 
+    adc_pin_init(analog_data.adc, GPIOC, PIN_1, analog_data.adc_channel[1], ADC_SMP_15); 
 
     // Set the ADC conversion sequence (called for each sequence entry) 
     // This only has an effect when scan mode is enabled 
-    adc_seq(ADC1, ADC_CHANNEL_10, ADC_SEQ_1); 
-    adc_seq(ADC1, ADC_CHANNEL_11, ADC_SEQ_2); 
+    adc_seq(analog_data.adc, analog_data.adc_channel[0], ADC_SEQ_1); 
+    adc_seq(analog_data.adc, analog_data.adc_channel[1], ADC_SEQ_2); 
 
     // Set the sequence length (called once) 
     // This only has an effect when scan mode is enabled 
-    adc_seq_len_set(ADC1, ADC_SEQ_2); 
+    adc_seq_len_set(analog_data.adc, ADC_SEQ_2); 
 
     // Turn the ADC on 
-    adc_on(ADC1); 
+    adc_on(analog_data.adc); 
 
     //================================================== 
 } 
@@ -92,32 +148,29 @@ void analog_test_init()
 
 void analog_test_app()
 {
-    // Local variables 
-    uint16_t adc_result[2]; 
-
 #if ADC_MODE_0 
 
     // Read ADC - Single read mode, scan mode disabled 
-    adc_result[0] = adc_read_single(ADC1, ADC_CHANNEL_10); 
-    adc_result[1] = adc_read_single(ADC1, ADC_CHANNEL_11); 
+    analog_data.adc_data[0] = adc_read_single(analog_data.adc, analog_data.adc_channel[0]); 
+    analog_data.adc_data[1] = adc_read_single(analog_data.adc, analog_data.adc_channel[1]); 
 
 #elif ADC_MODE_1 
 
     // Read ADC - Single read mode, scan mode enabled 
-    adc_scan_seq(ADC1, ADC_SEQ_2, adc_result); 
+    adc_scan_seq(analog_data.adc, ADC_SEQ_2, analog_data.adc_data); 
 
 #endif 
 
     // Display the result to the serial terminal 
-    uart_send_str(USART2, "ADC1_10: "); 
-    uart_send_integer(USART2, (int16_t)adc_result[0]); 
-    uart_send_spaces(USART2, 5); 
-    uart_send_str(USART2, "ADC1_11: "); 
-    uart_send_integer(USART2, (int16_t)adc_result[1]); 
-    uart_send_new_line(USART2); 
+    uart_send_str(analog_data.uart, "ADC1_10: "); 
+    uart_send_integer(analog_data.uart, (int16_t)analog_data.adc_data[0]); 
+    uart_send_spaces(analog_data.uart, 5); 
+    uart_send_str(analog_data.uart, "ADC1_11: "); 
+    uart_send_integer(analog_data.uart, (int16_t)analog_data.adc_data[1]); 
+    uart_send_new_line(analog_data.uart); 
 
     // Delay 
-    tim_delay_ms(TIM9, 1000); 
+    tim_delay_ms(analog_data.timer, ANALOG_TEST_DELAY_TIME); 
 }
 
 //=======================================================================================
