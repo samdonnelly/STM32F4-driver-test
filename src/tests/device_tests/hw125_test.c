c2220eca46b20a16fdf7fbcaa4905a9e54cd12c7
Sam <samueldonnelly11@gmail.com>
2025-09-26 17:42:11 -0600

    Added more commands to the hw125 driver test.

--- src/tests/device_tests/hw125_test.c
+++ src/tests/device_tests/hw125_test.c
@@ -27,9 +27,9 @@
 #define BUFF_SIZE 255 
 
 // User interface 
-#define HW125_NUM_DRIVER_CMDS 17            // Number of driver test commands for the user 
+#define HW125_NUM_DRIVER_CMDS 22            // Number of driver test commands for the user 
 #define HW125_NUM_CONT_CMDS 18              // Number of controller test commands for the user 
-#define CMD_SIZE 50                         // Max user command string length 
+#define CMD_SIZE 100                        // Max user command string length 
 
 // Controller testing 
 #define HW125_NUM_USER_CMDS 10              // Number of defined user commands for controller test 
@@ -55,23 +55,28 @@ typedef enum {
 // Prototypes 
 
 // User command functions 
-void mount_card(void);                // Mount the SD card 
-void unmount_card(void);              // Unmount the SD card 
-void card_capacity(void);             // Card Capacity 
-void file_check(void);                // Check files on card 
-void file_mkdir(void);                // Make a new directory 
-void file_open(void);                 // Open a file 
-void file_close(void);                // Close the open file 
-void file_put_string(void);           // Write to an open file using f_puts 
-void file_get_string(void);           // Read from an open file using f_gets 
-void file_printf(void);               // Write a formatted dtring 
-void file_write(void);                // Write to an open file using f_write 
-void file_read(void);                 // Read from an open file using using f_read 
-void file_seek(void);                 // Navigate the file 
-void file_rewind(void);               // Navigate to the beginning of the file 
-void file_fast_fwd(void);             // Navigate to the end of the file 
-void file_remove(void);               // Remove files from the drive 
-void display_buffer(void);            // Display the contents of 'buffer' 
+void mount_card(void);        // Mount the SD card 
+void unmount_card(void);      // Unmount the SD card 
+void card_capacity(void);     // Card Capacity 
+void file_check(void);        // Check existance of a file or sub-directory 
+void file_remove(void);       // Remove files from the drive 
+void file_mkdir(void);        // Make a new directory 
+void file_chdir(void);        // Change the current directory 
+void file_getcwd(void);       // Retrieve the current directory and drive 
+void file_opendir(void);      // Open a directory 
+void file_closedir(void);     // Close an open directory 
+void file_find(void);         // Find files on card 
+void file_open(void);         // Open a file 
+void file_close(void);        // Close the open file 
+void file_read(void);         // Read from an open file using using f_read 
+void file_write(void);        // Write to an open file using f_write 
+void file_seek(void);         // Navigate the file 
+void file_rewind(void);       // Navigate to the beginning of the file 
+void file_fast_fwd(void);     // Navigate to the end of the file 
+void file_get_string(void);   // Read from an open file using f_gets 
+void file_put_string(void);   // Write to an open file using f_puts 
+void file_printf(void);       // Write a formatted dtring 
+void display_buffer(void);    // Display the contents of 'buffer' 
 
 // Get user inputs 
 void get_input(
@@ -109,8 +114,9 @@ typedef struct hw125_test_record_s
 
     // User and data buffers 
     char cmd_buff[CMD_SIZE];              // Stores user commands 
+    char feedback_buff[BUFF_SIZE];        // Stores user feedback 
     char buffer[BUFF_SIZE];               // To store the data that we can read or write
-    char file_name_buff[CMD_SIZE];        // Stores file names input by the user 
+    char path_buff[CMD_SIZE];             // Stores path to file or directory 
     char file_mode_buff[CMD_SIZE];        // Stores file access modes input by the user 
     
     // File variables 
@@ -151,23 +157,33 @@ extern Disk_drvTypeDef disk;
 // User commands 
 static hw125_user_cmds_t cmd_table[HW125_NUM_DRIVER_CMDS] = 
 {
-    {"f_mount",     &mount_card}, 
-    {"f_unmount",   &unmount_card}, 
-    {"f_cap",       &card_capacity}, 
-    {"f_check",     &file_check}, 
-    {"f_mkdir",     &file_mkdir}, 
-    {"f_open",      &file_open}, 
-    {"f_close",     &file_close}, 
-    {"f_puts",      &file_put_string}, 
-    {"f_gets",      &file_get_string}, 
-    {"f_printf",    &file_printf}, 
-    {"f_write",     &file_write}, 
-    {"f_read",      &file_read}, 
-    {"f_lseek",     &file_seek}, 
-    {"f_rewind",    &file_rewind}, 
-    {"f_fastfwd",   &file_fast_fwd}, 
-    {"f_unlink",    &file_remove}, 
-    {"read_buffer", &display_buffer} 
+    // Volume Management and System Configuration commands 
+    {"mount",     &mount_card},
+    {"unmount",   &unmount_card},
+    {"getfree",   &card_capacity},
+    // File and Directory Management commands 
+    {"stat",      &file_check},
+    {"unlink",    &file_remove},
+    {"mkdir",     &file_mkdir},
+    {"chdir",     &file_chdir},
+    {"getcwd",    &file_getcwd},
+    // Directory Access commands 
+    {"opendir",   &file_opendir},
+    {"closedir",  &file_closedir},
+    {"find",      &file_find},
+    // File Access commands 
+    {"open",      &file_open},
+    {"close",     &file_close},
+    {"read",      &file_read},
+    {"write",     &file_write},
+    {"lseek",     &file_seek},
+    {"rewind",    &file_rewind},
+    {"fastfwd",   &file_fast_fwd},
+    {"gets",      &file_get_string},
+    {"puts",      &file_put_string},
+    {"printf",    &file_printf},
+    // Other commands 
+    {"read_buffer", &display_buffer}
 };
 
 //=======================================================================================
@@ -251,7 +267,7 @@ void hw125_test_app()
 {
     // Look for a user command 
     get_input(
-        "\r\nOperation >>> ", 
+        "\r\n>>> ", 
         hw125_data.cmd_buff,  
         CMD_SIZE, 
         &hw125_data.read_len, 
@@ -285,14 +301,20 @@ void mount_card(void)
     // Test to see if this will erase existing data 
     
     // Format the drive 
-    fresult = f_mkfs("", FM_EXFAT, 0, work, sizeof work); 
-    if (fresult != FR_OK)
+    hw125_data.fresult = f_mkfs("", FM_EXFAT, 0, hw125_data.work, sizeof(hw125_data.work));
+
+    if (hw125_data.fresult == FR_OK)
     {
-        uart_send_str(hw125_data.uart, "Error in formatting the SD Card.\r\n");
+        uart_send_str(hw125_data.uart, "SD Card formatted successfully.\r\n");
     }
     else
     {
-        uart_send_str(hw125_data.uart, "SD Card formatted successfully.\r\n"); 
+        // uart_send_str(hw125_data.uart, "Error in formatting the SD Card.\r\n");
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\nProblem occurred --> FRESULT == %u\r\n",
+                 (uint8_t)hw125_data.fresult);
+        uart_send_str(hw125_data.uart, hw125_data.feedback_buff);
     }
 
 #endif
@@ -333,20 +355,12 @@ void mount_card(void)
 // Unmount card 
 void unmount_card(void) 
 {
-    // Unmount the volume 
-    hw125_data.fresult = f_unmount(""); 
+    // Unmount the volume and clear the initialization status so it can be re-mounted. 
+    hw125_data.fresult = f_unmount("");
+    disk.is_initialized[0] = CLEAR;
 
-    // Clear the initialization status so it can be re-mounted 
-    disk.is_initialized[0] = CLEAR; 
-
-    if (hw125_data.fresult == FR_OK) 
-    {
-        uart_send_str(hw125_data.uart, "\nVolume unmounted successfully.\r\n"); 
-    }
-    else 
-    {
-        uart_send_str(hw125_data.uart, "\nError in unmounting volume.\r\n");
-    }
+    (hw125_data.fresult == FR_OK) ? uart_send_str(hw125_data.uart, "\nVolume unmounted successfully.\r\n") : 
+                                    uart_send_str(hw125_data.uart, "\nError in unmounting volume.\r\n");
 }
 
 
@@ -374,8 +388,208 @@ void card_capacity(void)
 }
 
 
-// Check files on the card 
+// Check existance of a file or sub-directory 
 void file_check(void)
+{
+    // Get and format the file or directory 
+    get_input(
+        "\nFile or directory to check: ", 
+        hw125_data.path_buff, 
+        CMD_SIZE, 
+        &hw125_data.read_len, 
+        FORMAT_FILE_STRING); 
+
+    // Check for the existance of the specified file or directory 
+    hw125_data.fresult = f_stat(hw125_data.path_buff, &hw125_data.fno);
+
+    if (hw125_data.fresult == FR_OK)
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\n'%s' exists",
+                 hw125_data.path_buff);
+    }
+    else if (hw125_data.fresult == FR_NO_FILE)
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\n'%s' does not exist",
+                 hw125_data.path_buff);
+    }
+    else
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\nProblem occurred --> FRESULT == %u\r\n",
+                 (uint8_t)hw125_data.fresult);
+    }
+
+    uart_send_str(hw125_data.uart, hw125_data.feedback_buff);
+}
+
+
+// Remove files on card 
+void file_remove(void) 
+{
+    // Get and format the file position 
+    get_input(
+        "\nFile to remove: ", 
+        hw125_data.path_buff, 
+        CMD_SIZE, 
+        &hw125_data.read_len, 
+        FORMAT_FILE_STRING); 
+
+    // Attempt to remove the specified file 
+    hw125_data.fresult = f_unlink(hw125_data.path_buff); 
+
+    if (hw125_data.fresult != FR_OK) 
+    {
+        uart_send_str(hw125_data.uart, "\r\nFailed to remove "); 
+        uart_send_str(hw125_data.uart, hw125_data.path_buff); 
+        uart_send_new_line(hw125_data.uart); 
+    }
+}
+
+
+// Make a new directory 
+void file_mkdir(void)
+{
+    // Get and format the directory path string 
+    get_input(
+        "\nDirectory: ", 
+        hw125_data.buffer, 
+        BUFF_SIZE, 
+        &hw125_data.read_len, 
+        FORMAT_FILE_STRING); 
+
+    // Write to the file 
+    hw125_data.fresult = f_mkdir(hw125_data.buffer); 
+}
+
+
+// Change the current directory 
+void file_chdir(void)
+{
+    // Get and format the directory name 
+    get_input(
+        "\nDirectory to go to: ", 
+        hw125_data.path_buff, 
+        CMD_SIZE, 
+        &hw125_data.read_len, 
+        FORMAT_FILE_STRING);
+
+    // Change the directory 
+    hw125_data.fresult = f_chdir(hw125_data.path_buff);
+
+    if (hw125_data.fresult == FR_OK)
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\nNew directory: '%s'\r\n",
+                 hw125_data.path_buff);
+    }
+    else
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\nProblem occurred --> FRESULT == %u\r\n",
+                 (uint8_t)hw125_data.fresult);
+    }
+
+    uart_send_str(hw125_data.uart, hw125_data.feedback_buff);
+}
+
+
+// Retrieve the current directory and drive 
+void file_getcwd(void)
+{
+    if (hw125_data.file_sys.fs_type == FS_EXFAT)
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\nCan't fetch current directory of exFAT volumes. getcwd return root\r\n");
+        uart_send_str(hw125_data.uart, hw125_data.feedback_buff);
+    }
+
+    // Get the current directory 
+    hw125_data.fresult = f_getcwd(hw125_data.path_buff, CMD_SIZE);
+
+    if (hw125_data.fresult == FR_OK)
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\nCurrent directory: '%s'\r\n",
+                 hw125_data.path_buff);
+    }
+    else
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\nProblem occurred --> FRESULT == %u\r\n",
+                 (uint8_t)hw125_data.fresult);
+    }
+
+    uart_send_str(hw125_data.uart, hw125_data.feedback_buff);
+}
+
+
+// Open a directory 
+void file_opendir(void)
+{
+    // Get and format the directory name 
+    get_input(
+        "\nDirectory to open: ", 
+        hw125_data.path_buff, 
+        CMD_SIZE, 
+        &hw125_data.read_len, 
+        FORMAT_FILE_STRING);
+
+    // Open the specified directory 
+    hw125_data.fresult = f_opendir(&hw125_data.dj, hw125_data.path_buff);
+
+    if (hw125_data.fresult == FR_OK)
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\n'%s' opened",
+                 hw125_data.path_buff);
+    }
+    else
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\nProblem occurred --> FRESULT == %u\r\n",
+                 (uint8_t)hw125_data.fresult);
+    }
+
+    uart_send_str(hw125_data.uart, hw125_data.feedback_buff);
+}
+
+
+// Close an open directory 
+void file_closedir(void)
+{
+    // Close the current directory 
+    hw125_data.fresult = f_closedir(&hw125_data.dj);
+
+    if (hw125_data.fresult == FR_OK)
+    {
+        uart_send_str(hw125_data.uart, "\r\nDirectory closed.");
+        file_getcwd();
+    }
+    else
+    {
+        snprintf(hw125_data.feedback_buff,
+                 BUFF_SIZE,
+                 "\r\nProblem occurred --> FRESULT == %u\r\n",
+                 (uint8_t)hw125_data.fresult);
+        uart_send_str(hw125_data.uart, hw125_data.feedback_buff);
+    }
+}
+
+
+// Check files on the card 
+void file_find(void)
 {
     // Get and format the directory to check 
     get_input(
@@ -402,29 +616,13 @@ void file_check(void)
 }
 
 
-// Make a new directory 
-void file_mkdir(void)
-{
-    // Get and format the directory path string 
-    get_input(
-        "\nDirectory: ", 
-        hw125_data.buffer, 
-        BUFF_SIZE, 
-        &hw125_data.read_len, 
-        FORMAT_FILE_STRING); 
-
-    // Write to the file 
-    hw125_data.fresult = f_mkdir(hw125_data.buffer); 
-}
-
-
 // Open a file 
 void file_open(void)
 {
     // Get and format the file name 
     get_input(
         "\nFile to open: ", 
-        hw125_data.file_name_buff, 
+        hw125_data.path_buff, 
         CMD_SIZE, 
         &hw125_data.read_len, 
         FORMAT_FILE_STRING); 
@@ -438,7 +636,7 @@ void file_open(void)
         FORMAT_FILE_MODE); 
 
     // Open a file (and create if it doesn't exist) 
-    hw125_data.fresult = f_open(&hw125_data.file, hw125_data.file_name_buff, hw125_data.access_mode); 
+    hw125_data.fresult = f_open(&hw125_data.file, hw125_data.path_buff, hw125_data.access_mode); 
 }
 
 
@@ -446,88 +644,6 @@ void file_open(void)
 void file_close(void) 
 {
     f_close(&hw125_data.file); 
-}
-
-
-// Write to an open file using f_puts 
-void file_put_string(void)
-{
-    // Get and format the file string 
-    get_input(
-        "\nFile string: ", 
-        hw125_data.buffer, 
-        BUFF_SIZE, 
-        &hw125_data.read_len, 
-        FORMAT_FILE_STRING); 
-
-    // Write a string 
-    f_puts(hw125_data.buffer, &hw125_data.file); 
-}
-
-
-// Read from an open file using f_gets 
-void file_get_string(void)
-{
-    // Get and format the read size (bytes) 
-    get_input(
-        "\nRead size (bytes): ", 
-        hw125_data.buffer, 
-        BUFF_SIZE, 
-        &hw125_data.read_len, 
-        FORMAT_FILE_NUM); 
-
-    // Read from the file 
-    f_gets(hw125_data.buffer, hw125_data.read_len, &hw125_data.file); 
-}
-
-
-// Write a formatted dtring 
-void file_printf(void)
-{
-    QWORD fmt_value; 
-
-    // Get and format the formatted string integer 
-    get_input(
-        "\nInteger: ", 
-        hw125_data.buffer, 
-        BUFF_SIZE, 
-        &fmt_value, 
-        FORMAT_FILE_NUM); 
-    
-    // Get and format the formated string 
-    get_input(
-        "\nFormatted string: ", 
-        hw125_data.buffer, 
-        BUFF_SIZE, 
-        &hw125_data.read_len, 
-        FORMAT_FILE_STRING); 
-
-    // Write the formatted string to the file 
-    if (f_printf(&hw125_data.file, hw125_data.buffer, (uint16_t)fmt_value) < 0) 
-    {
-        uart_send_str(hw125_data.uart, "\nfailure\r\n"); 
-    }
-}
-
-
-// Write to an open file using f_write 
-void file_write(void) 
-{
-    // Get and format the file string 
-    get_input(
-        "\nFile string: ", 
-        hw125_data.buffer, 
-        BUFF_SIZE, 
-        &hw125_data.read_len, 
-        FORMAT_FILE_STRING); 
-
-    // Write to the file 
-    hw125_data.fresult = f_write(&hw125_data.file, 
-                                 hw125_data.buffer, 
-                                 strlen(hw125_data.buffer), 
-                                 &hw125_data.bw); 
-
-    // Indicate if write failed 
 }
 
 
@@ -549,6 +665,27 @@ void file_read(void)
                                 &hw125_data.br); 
 
     // Indicate if the read failed 
+}
+
+
+// Write to an open file using f_write 
+void file_write(void) 
+{
+    // Get and format the file string 
+    get_input(
+        "\nFile string: ", 
+        hw125_data.buffer, 
+        BUFF_SIZE, 
+        &hw125_data.read_len, 
+        FORMAT_FILE_STRING); 
+
+    // Write to the file 
+    hw125_data.fresult = f_write(&hw125_data.file, 
+                                 hw125_data.buffer, 
+                                 strlen(hw125_data.buffer), 
+                                 &hw125_data.bw); 
+
+    // Indicate if write failed 
 }
 
 
@@ -582,25 +719,63 @@ void file_fast_fwd(void)
 }
 
 
-// Remove files on card 
-void file_remove(void) 
+// Read from an open file using f_gets 
+void file_get_string(void)
 {
-    // Get and format the file position 
+    // Get and format the read size (bytes) 
     get_input(
-        "\nFile to remove: ", 
-        hw125_data.file_name_buff, 
-        CMD_SIZE, 
+        "\nRead size (bytes): ", 
+        hw125_data.buffer, 
+        BUFF_SIZE, 
+        &hw125_data.read_len, 
+        FORMAT_FILE_NUM); 
+
+    // Read from the file 
+    f_gets(hw125_data.buffer, hw125_data.read_len, &hw125_data.file); 
+}
+
+
+// Write to an open file using f_puts 
+void file_put_string(void)
+{
+    // Get and format the file string 
+    get_input(
+        "\nFile string: ", 
+        hw125_data.buffer, 
+        BUFF_SIZE, 
         &hw125_data.read_len, 
         FORMAT_FILE_STRING); 
 
-    // Attempt to remove the specified file 
-    hw125_data.fresult = f_unlink(hw125_data.file_name_buff); 
+    // Write a string 
+    f_puts(hw125_data.buffer, &hw125_data.file); 
+}
 
-    if (hw125_data.fresult != FR_OK) 
+
+// Write a formatted dtring 
+void file_printf(void)
+{
+    QWORD fmt_value; 
+
+    // Get and format the formatted string integer 
+    get_input(
+        "\nInteger: ", 
+        hw125_data.buffer, 
+        BUFF_SIZE, 
+        &fmt_value, 
+        FORMAT_FILE_NUM); 
+    
+    // Get and format the formated string 
+    get_input(
+        "\nFormatted string: ", 
+        hw125_data.buffer, 
+        BUFF_SIZE, 
+        &hw125_data.read_len, 
+        FORMAT_FILE_STRING); 
+
+    // Write the formatted string to the file 
+    if (f_printf(&hw125_data.file, hw125_data.buffer, (uint16_t)fmt_value) < 0) 
     {
-        uart_send_str(hw125_data.uart, "\r\nFailed to remove "); 
-        uart_send_str(hw125_data.uart, hw125_data.file_name_buff); 
-        uart_send_new_line(hw125_data.uart); 
+        uart_send_str(hw125_data.uart, "\nfailure\r\n"); 
     }
 }
 
